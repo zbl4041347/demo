b412bd8b1186b56a78e45d33063b1f57f2dea93f
zhabinglong <zbl4041347@126.com>
2022-11-19 22:51:46 +0800

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 }
