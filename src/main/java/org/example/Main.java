a137cc38021e54b04769b273d4df66536875f25c
zhabinglong <zbl4041347@126.com>
2022-11-19 22:53:17 +0800

    hot-fix commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
+        System.out.println("Hello git4");
     }
 }
